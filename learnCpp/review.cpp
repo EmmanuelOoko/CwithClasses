21891766b3432b6d757e026fe42a16dd6554520e
Emmanuel <emmanuel.ooko@strathmore.edu>
2022-03-01 13:47:31 +0300

    manipulating vectors, loops and user inputs

--- learnCpp/review.cpp
+++ learnCpp/review.cpp
@@ -3,12 +3,37 @@
 using namespace std;
 
 void vectorManipulation(){
-    vector<int> myVector = {12, 31, 14};
+    vector<int> myVector(4);
+
+    cout << "Enter " << myVector[0] << ": \n";
+    cin >> myVector[0];
+
+    cout << "Enter " << myVector[1] << ": \n";
+    cin >> myVector[1];
+
+    cout << "Enter " << myVector[2] << ": \n";
+    cin >> myVector[2];
+
+    cout << "Enter " << myVector[3] << ": \n";
+    cin >> myVector[3];
+
+    //Listing all the elements in the vector before pop_back()
+    for (int num = 0; num < myVector.size(); num++){
+           cout << myVector[num] << "\n";
+    }
+
     cout<< "First Size: " << myVector.size() << "\n";
     myVector.pop_back();
     cout<< "Second Size: " << myVector.size() << "\n";
     myVector.push_back(51);
-    cout<< "Updated Size: " << myVector.size() << "\n";
+    myVector.push_back(54);
+    myVector.push_back(71);
+    cout << "Updated Size: " << myVector.size() << "\n";
+
+    //Listing all the elements in the vector
+    for (int i = 0; i < myVector.size(); i++){
+           cout << myVector[i] << "\n";
+    }
 }
 
 int main (){
