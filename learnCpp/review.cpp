c236b04111698e7cba1451c2e5e7556e616c7bb9
Emmanuel <emmanuel.ooko@strathmore.edu>
2022-03-01 12:11:04 +0300

    make vectorManipulation method void, add main method

--- learnCpp/review.cpp
+++ learnCpp/review.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 using namespace std;
 
-int vectorManipulation(){
+void vectorManipulation(){
     vector<int> myVector = {12, 31, 14};
     cout<< "First Size: " << myVector.size() << "\n";
     myVector.pop_back();
     cout<< "Second Size: " << myVector.size() << "\n";
     myVector.push_back(51);
     cout<< "Updated Size: " << myVector.size() << "\n";
+}
 
+int main (){
+    vectorManipulation();
 }
